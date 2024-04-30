ba878e70f598cf336d2c659f2380e96e5ac18cd5
Anastasiya Rossova <rossovaay@ya.ru>
2024-04-30 16:51:29 +0900

    добавлена фильтрация поиска

--- main.py
+++ main.py
@@ -2,32 +2,68 @@ from src.hh import HeadHunterAPI
 from src.vacancy import Vacancy
 from src.json_saver import JSONSaver
 
-# Создание экземпляра класса для работы с API сайтов с вакансиями
-hh_api = HeadHunterAPI()
 
-# Получение вакансий с hh.ru в формате JSON
-hh_api.load_vacancies("Python")
+def search(search_query):
+    # Создание экземпляра класса для работы с API сайтов с вакансиями
+    hh_api = HeadHunterAPI()
 
-# Преобразование набора данных из JSON в список объектов
-vacancies_list = Vacancy.cast_to_object_list(hh_api.vacancies)
+    # Получение вакансий с hh.ru в формате JSON
+    hh_api.load_vacancies(search_query)
+    # Преобразование набора данных из JSON в список объектов
+    vacancies_list = Vacancy.cast_to_object_list(hh_api.vacancies)
+    print(vacancies_list)
+    # Сохранение информации о вакансиях в файл
+    json_saver = JSONSaver()
+    for vacancy in vacancies_list:
+        json_saver.add_vacancy(vacancy)
+        json_saver.commit("vacancies")
+    # json_saver.delete_vacancy(vacancy)
+    return vacancies_list
 
-# Сохранение информации о вакансиях в файл
-json_saver = JSONSaver()
-json_saver.add_vacancy(vacancy)
-json_saver.delete_vacancy(vacancy)
+
+def filter_vacancies(vacancies_list, filter_words):
+    if filter_words:
+        filter_words = [x.lower() for x in filter_words]
+        filtered_vacancies = []
+        for vacancy in vacancies_list:
+            if vacancy.name.lower() in filter_words:
+                filtered_vacancies.append(vacancy)
+        return filtered_vacancies
+    return vacancies_list
+
+
+def get_vacancies_by_salary(filtered_vacancies, min_range):
+    ranged_vacancies = []
+    for vacancy in filtered_vacancies:
+        if vacancy.salary_to:
+            if int(vacancy.salary_to) >= min_range:
+                ranged_vacancies.append(vacancy)
+    return ranged_vacancies
+
+
+def sort_vacancies(ranged_vacancies):
+    return sorted(ranged_vacancies, key=lambda vacancy: vacancy.salary_to, reverse=True)
+
+
+def get_top_vacancies(sorted_vacancies, top_n):
+    return sorted_vacancies[:top_n]
+
+
+def print_vacancies(top_vacancies):
+    for vacancy in top_vacancies:
+        print(repr(vacancy))
+
 
 # Функция для взаимодействия с пользователем
 def user_interaction():
-    platforms = ["HeadHunter"]
     search_query = input("Введите поисковый запрос: ")
+    vacancies_list = search(search_query)
     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
     filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
+    min_range = int(input("Введите минимальную зарплату: "))  # Пример: 100000
 
     filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
-
-    ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
-
+    ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, min_range)
     sorted_vacancies = sort_vacancies(ranged_vacancies)
     top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
     print_vacancies(top_vacancies)
--- src/vacancy.py
+++ src/vacancy.py
@@ -8,14 +8,28 @@ class AbstractVacancy(ABC):
 
 
 class Vacancy:
-    def __init__(self, _id, salary, name, area, employer_name, requirement, json):
+    def __init__(self, _id, salary_from, salary_to, name, area,
+                 employer_name, requirement, json):
         self._id = _id
-        self._salary = salary
+        self._salary_from = salary_from
+        self._salary_to = salary_to
         self._name = name
         self._area = area
         self._employer_name = employer_name
         self._requirement = requirement
         self._json = json
+
+    @property
+    def json(self) -> str:
+        return self._json
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def salary_to(self) -> str:
+        return self._salary_to
 
     def __repr__(self):
         return f'Вакансия [{self._id}] {self._name}: {self._name}'
@@ -26,12 +40,13 @@ class Vacancy:
         for vacancy in vacancies:
             new_vacancy = Vacancy(
                 vacancy.get("_id"),
-                vacancy.get("salary"),
+                vacancy.get("salary").get("from") if vacancy.get("salary") else "",
+                vacancy.get("salary").get("to") if vacancy.get("salary") else "",
                 vacancy.get("name"),
                 vacancy.get("area"),
-                vacancy.get("employer").get("name"),
+                vacancy.get("employer").get("name") if vacancy.get("employer") else "",
                 vacancy.get("requirement") if vacancy.get("requirement") else "",
                 vacancy
             )
-        vacancies_list.append(new_vacancy)
+            vacancies_list.append(new_vacancy)
         return vacancies_list
